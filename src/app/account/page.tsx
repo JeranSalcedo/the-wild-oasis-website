6c360aeb1ef7b37866c70ab3ccf18c16dd94cf96
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-06-05 11:51:53 +0800

    chore(content): minor updates to route pages (formatting/content)

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,3 +1,9 @@
+export const metadata = {
+	title: "Account",
+	description:
+		"Manage your account settings and preferences at The Wild Oasis",
+};
+
 export default function Page() {
 	return <h1>Account page</h1>;
 }
