3b10388578d59093b64a0905f39155a6c1ecf4e5
JeranSalcedo <janraymondsalcedo@gmail.com>
2026-06-06 19:27:23 +0800

    feat(cabins): add cabin feature data types and mapper

---
+++ src/features/cabins/types/cabin-data.types.ts
@@ -0,0 +1,10 @@
+export type CabinData = {
+	id: number;
+	created_at: string;
+	name: string;
+	description: string;
+	image_url: string;
+	max_capacity: number;
+	base_price: number;
+	discount: number;
+};
---
+++ src/features/cabins/types/cabin.types.ts
@@ -0,0 +1,10 @@
+export type Cabin = {
+	id: number;
+	createdAt: string;
+	name: string;
+	description: string;
+	imageUrl: string;
+	maxCapacity: number;
+	basePrice: number;
+	discount: number;
+};
